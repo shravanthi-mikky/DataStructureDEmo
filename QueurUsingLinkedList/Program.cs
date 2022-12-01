9de34f00d3be0b663c369a9803d8f97543ec4a3f
shravanthi-mikky <shravanthi27041996@gmail.com>
2022-12-01 15:07:58 +0530

    [shravanthi] - Basic structure of program

---
+++ QueurUsingLinkedList/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace QueurUsingLinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            QueueUsingLList linkedListQueue = new QueueUsingLList();
+            linkedListQueue.Enqueue(56);
+            linkedListQueue.Enqueue(30);
+            linkedListQueue.Enqueue(70);
+            linkedListQueue.Display();// this.top=this.top.next
+            Console.ReadKey();
+        }
+    }
+}
